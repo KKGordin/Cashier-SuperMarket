d3b5f7fe3c3cb30e506d40bba016825b5bbc09cd
Kelver Mata <kaykyto-amaral@hotmail.com>
2024-11-26 21:20:12 -0300

    new function

--- cashier.py
+++ cashier.py
@@ -4,9 +4,6 @@
 import os
 import time
 
-print("======= SuperMarket =======")
-start = input("Pressione [Enter] para começar!")
-
 Banco_De_Dados = [
     {"id":2627, "nome": "Arroz 5kg", "preco": 36.90},
     {"id":2923, "nome": "Feijão 1kg", "preco": 6.90},
@@ -54,97 +51,117 @@ def pagamento_card(card):
             else:
                 print("[ERROR] Não autorizado!")
     
-            
-        
+while True: 
     
-if start == "":
-    print("""
-    Opções:
-    [1] Adicionar produtos às compras
-    [2] Extrato do dia
-    [3] Desligar caixa""")
-    escolha = int(input(">>> "))
+    os.system("cls")
+
+    print("========== SuperMarket ==========")
+    start = input("Pressione [Enter] para começar!")
         
-if escolha == 1:
-    while True:
+    if start == "":
+        print("""
+Opções:
+
+[1] Adicionar produtos às compras
+[2] Extrato do dia
+[3] Desligar caixa""")
+        escolha = int(input(">>> "))
             
-        # Mostrando a lista
+        if escolha == 1:
+            while True:
+                    
+                # Mostrando a lista
+                os.system("cls")
+                
+                if len(compras) > 0:
+                    for c in compras:
+                        print("Nome:",c["nome"], "| Preço: R$",c["preco"])
+                        
+                # Finalizando:     
+                ad_produto = int(input("[0]Finish [1]Remove | ID do produto: "))
+                if ad_produto == 0:        
+                    break
+                # Removendo itens da lista:   
+                elif ad_produto == 1:
+                    remove = int(input("[0]Remove Last | Item number: "))
+                    if remove == 0:
+                        ad_produto = compras.pop()
+                        ad_produto = last_item.pop()
+                    
+                    else:
+                        ad_produto = compras.pop(remove - 1)
+                        ad_produto = last_item.pop(remove - 1)
+                
+                # Adicionando itens na lista:
+                else:
+                    for produtos in Banco_De_Dados:
+                        if produtos["id"] == ad_produto:
+                            compras.append({
+                                "nome": produtos["nome"],
+                                "preco": produtos["preco"]
+                            })
+                    
+                    
+                    last_item.append(compras[-1])
+        
+        elif escolha == 3:
+            print("Desligando...")
+            time.sleep(5)
+            break
+        
+        for select_prod in last_item:
+            soma_produtos.append(select_prod["preco"])
+                    
         os.system("cls")
         
-        if len(compras) > 0:
-            for c in compras:
-                print("Nome:",c["nome"], "| Preço: R$",c["preco"])
-                
-        # Finalizando:     
-        ad_produto = int(input("[0]Finish [1]Remove | ID do produto: "))
-        if ad_produto == 0:        
-            break
-        # Removendo itens da lista:   
-        elif ad_produto == 1:
-            remove = int(input("[0]Remove Last | Item number: "))
-            if remove == 0:
-                ad_produto = compras.pop()
-                ad_produto = last_item.pop()
-            
-            else:
-                ad_produto = compras.pop(remove - 1)
-                ad_produto = last_item.pop(remove - 1)
+        # Forma de pagamento
         
-        # Adicionando itens na lista:
-        else:
-            for produtos in Banco_De_Dados:
-                if produtos["id"] == ad_produto:
-                    compras.append({
-                        "nome": produtos["nome"],
-                        "preco": produtos["preco"]
-                    })
-            
-            
-            last_item.append(compras[-1])
-    
-    for select_prod in last_item:
-        soma_produtos.append(select_prod["preco"])
-                
-    os.system("cls")
-    
-    # Forma de pagamento
-    
-    print("===== Forma de pagamento =====")
-    soma = sum(soma_produtos)
-    print("A compra ficou por: R${:.2f}".format(soma))
-    print("""
-    [1] Dinheiro
-    [2] Pix
-    [3] Cartão Débito
-    [4] Cartão Crédito""")
-    opc = int(input("Digite sua opção: "))
-    
-    if opc == 1:
-        print("Processando pagamento...")
-        time.sleep(5)
-        print("Compra finalizada com sucesso!")
-    
-    elif opc == 2:
-        print("Gerando QR Code...")
-        time.sleep(2)
-        print("Processando pagamento...")
-        time.sleep(5)
-        print("Compra finalizada com sucesso!")
-        
-    elif opc == 3:
-        pagamento_card(cartao_debito)
-        
-        
-    elif opc == 4:
-        print("Você pode parcelar em até 6x sem juros!")
-        parcela = int(input("Em quantas vezes você deseja parcelar: "))
+        print("===== Forma de pagamento =====")
         soma = sum(soma_produtos)
-        res = soma / parcela
-        print("Cada parcela ficou por {:.2f}".format(res))
-        pagamento_card(cartao_credito)
+        print("A compra ficou por: R${:.2f}".format(soma))
+        print("""
+        [1] Dinheiro
+        [2] Pix
+        [3] Cartão Débito
+        [4] Cartão Crédito""")
+        opc = int(input("Digite sua opção: "))
         
-else:
-    print("[ERROR] Você deve pressionar [Enter] para começar!")
+        if opc == 1:
+            print("Processando pagamento...")
+            time.sleep(5)
+            print("Compra finalizada com sucesso!")
+            time.sleep(6)
+        
+        elif opc == 2:
+            print("Gerando QR Code...")
+            time.sleep(2)
+            print("Processando pagamento...")
+            time.sleep(5)
+            print("Compra finalizada com sucesso!")
+            time.sleep(6)
+            
+        elif opc == 3:
+            pagamento_card(cartao_debito)
+            time.sleep(6)
+            
+            
+        elif opc == 4:
+            print("Você pode parcelar em até 6x sem juros!")
+            print("________________________________")
+            parcela = int(input("Em quantas vezes você deseja parcelar: "))
+            soma = sum(soma_produtos)
+            res = soma / parcela
+            print("|== Cada parcela ficou por {:.2f} ==|".format(res))
+            pagamento_card(cartao_credito)
+            time.sleep(6)
+
+        if escolha == 2:
+            ...
+     
+            
+    else:
+        print("[ERROR] Você deve pressionar [Enter] para começar!")
+        time.sleep(3)
         
 
         
